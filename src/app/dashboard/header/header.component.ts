30d90c4f038e9b051bdf3dab9a07b0b22fcb3e72
suchit-absolve-innovations <106303338+suchit-absolve-innovations@users.noreply.github.com>
2026-01-20 13:19:16 +0530

    set code

--- src/app/dashboard/header/header.component.ts
+++ src/app/dashboard/header/header.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  user: any = null;
+
+  ngOnInit(): void {
+    const storedUser = localStorage.getItem('user'); // ya API response se
+    this.user = storedUser ? JSON.parse(storedUser) : null;
+  }
+
+  get userName(): string {
+    return this.user?.name?.trim() || 'User';
+  }
+
+  get profileImage(): string {
+    return this.user?.profilePicUrl?.trim()
+      ? this.user.profilePicUrl
+      : 'assets/images/c1_1.webp';
+  }
+
+  onImageError(event: Event) {
+    (event.target as HTMLImageElement).src = 'assets/images/c1_1.webp';
+  }
 }
