a97b75d84df48eff96d78f65913fe63f8db95dc8
suchit-absolve-innovations <106303338+suchit-absolve-innovations@users.noreply.github.com>
2026-01-21 12:35:53 +0530

    set code

--- src/app/loan/loan-routing.module.ts
+++ src/app/loan/loan-routing.module.ts
@@ -9,6 +9,8 @@ import { EmploymentComponent } from './employment/employment.component';
 import { LoanApplicationHomeComponent } from './loan-application-home/loan-application-home.component';
 import { EKYCVerificationComponent } from './ekyc/e-kyc-verification/e-kyc-verification.component';
 import { BankVerificationComponent } from './bank/bank-verification/bank-verification.component';
+import { SalarySlipComponent } from './documents/salary-slip/salary-slip.component';
+import { AddressProofComponent } from './documents/address-proof/address-proof.component';
 const routes: Routes = [
   { path: '', component: LoanApplicationHomeComponent },
   { path: 'calculator', component: CalculatorComponent },
@@ -18,7 +20,9 @@ const routes: Routes = [
   { path: 'documents', component: DocumentsComponent },
   { path: 'disbursal', component: DisbursalComponent },
   { path: 'ekyc-verification', component: EKYCVerificationComponent},
-  { path: 'bank-verification', component:BankVerificationComponent}
+  { path: 'bank-verification', component:BankVerificationComponent},
+  { path: ' salary-slip', component:SalarySlipComponent},
+  { path: 'address-proof', component:AddressProofComponent}
 ];
 
 @NgModule({
