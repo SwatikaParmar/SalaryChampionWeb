a22e576e4abdd00902216b9eb6a28a1e333529c7
sagemember27 <44390731+sagemember27@users.noreply.github.com>
2026-04-16 14:20:55 +0530

    set code

--- src/app/landing/terms-condition/terms-condition.component.ts
+++ src/app/landing/terms-condition/terms-condition.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-terms-condition',
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrl: './terms-condition.component.css'
 })
 export class TermsConditionComponent {
+isMenuOpen = false;
 
+  loanAmount = 50000;
+  tenure = 15; // days
+
+  interestRate = 1; // 1% daily
+  processingFeeRate = 10; // %
+  gstRate = 18; // %
+
+  interest = 0;
+  processingFee = 0;
+  gst = 0;
+  totalPayment = 0;
+
+  ngOnInit(): void {
+    this.calculateLoan();
+  }
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  calculateLoan() {
+
+    const P = this.loanAmount;
+    const days = this.tenure;
+
+    // 🔥 Interest (daily)
+    this.interest = (P * this.interestRate / 100) * days;
+
+    // 🔥 Processing Fee
+    this.processingFee = (P * this.processingFeeRate) / 100;
+
+    // 🔥 GST on PF
+    this.gst = (this.processingFee * this.gstRate) / 100;
+
+    // 🔥 Total
+    this.totalPayment = P + this.interest + this.processingFee + this.gst;
+
+  }
 }
