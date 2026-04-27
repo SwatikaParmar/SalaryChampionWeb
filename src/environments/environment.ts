0f3f3c1a1cab4ee7f16547c8c6a51b6afd60ac30
suchit-absolve-innovations <106303338+suchit-absolve-innovations@users.noreply.github.com>
2026-04-27 10:07:51 +0530

    set code

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
- apiUrl: "https://dyjrqdgw6rhwz.cloudfront.net/",
-  rootPathUrl: "https://dyjrqdgw6rhwz.cloudfront.net/",
+apiUrl: "https://d1sm1pbrlvdqo6.cloudfront.net/",
+  rootPathUrl: "https://d1sm1pbrlvdqo6.cloudfront.net/",
   firebase : {
   apiKey: "AIzaSyA4u51jhF9KijKON-xrvC7YgHIcpAZsEco",
   authDomain: "salary-champion.firebaseapp.com",
